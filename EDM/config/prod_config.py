6f93eadf54da551c5dfd83202495af635571945a
Johan <114432256+WizardSuey@users.noreply.github.com>
2025-05-25 23:56:34 +0700

    Update prod_config.py

--- EDM/config/prod_config.py
+++ EDM/config/prod_config.py
@@ -5,7 +5,7 @@ UTD_DOCS_UPLOAD_FOLDER = 'EDM/static/utd_docs'
 CONTRACT_DOCS_UPLOAD_FOLDER = 'EDM/static/contract_docs'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-DATABASE_HOST = 'postgresql://dpg-d0pk74muk2gs739ogvv0-a.oregon-postgres.render.com'
+DATABASE_HOST = 'dpg-d0pk74muk2gs739ogvv0-a'
 DATABASE_PORT = 5432
 DATABASE_USER = 'ergodocsbd_user'
 DATABASE_PASSWORD = 'o6RIa0OBaLrqNDplHh3LPNLzgK2p0hWb'
