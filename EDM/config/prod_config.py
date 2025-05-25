a210199ee38200fb96c0307214fde5bd7e9ef3fc
Ilya <dorogino@gmail.com>
2025-05-25 23:24:09 +0700

    c

--- EDM/config/prod_config.py
+++ EDM/config/prod_config.py
@@ -5,11 +5,11 @@ UTD_DOCS_UPLOAD_FOLDER = 'EDM/static/utd_docs'
 CONTRACT_DOCS_UPLOAD_FOLDER = 'EDM/static/contract_docs'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-DATABASE_HOST = 'localhost'
+DATABASE_HOST = 'postgresql://ergodocsbd_user:o6RIa0OBaLrqNDplHh3LPNLzgK2p0hWb@dpg-d0pk74muk2gs739ogvv0-a.oregon-postgres.render.com/ergodocsbd'
 DATABASE_PORT = 5432
-DATABASE_USER = 'postgres'
-DATABASE_PASSWORD = '48BitColor48'
-DATABASE_NAME = 'EDM'
+DATABASE_USER = 'ergodocsbd_user'
+DATABASE_PASSWORD = 'o6RIa0OBaLrqNDplHh3LPNLzgK2p0hWb'
+DATABASE_NAME = 'ergodocsbd'
 
 # HOST = '0.0.0.0'
 HOST = '0.0.0.0'
