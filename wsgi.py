f9dcc8ea852e9575cbbb64a3f6d41bc683623b98
Johan <114432256+WizardSuey@users.noreply.github.com>
2025-05-25 23:34:05 +0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from EDM import create_app, socketio
 if __name__ == '__main__':
     app = create_app(config='prod')
     print(app.url_map)
-    socketio.run(app, host=app.config['HOST'], port=app.config['PORT'])
+    socketio.run(app, host=app.config['HOST'])
