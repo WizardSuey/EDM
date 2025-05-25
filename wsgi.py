9357615b34a2509fbcd5eaf1b12a842e8693928c
Johan <114432256+WizardSuey@users.noreply.github.com>
2025-05-26 00:54:11 +0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from EDM import create_app, socketio
 if __name__ == '__main__':
     app = create_app(config='prod')
     print(app.url_map)
-    socketio.run(app, host=app.config['HOST'], allow_unsafe_werkzeug=True)
+    socketio.run(app, host=app.config['HOST'], port=app.config['PORT'], allow_unsafe_werkzeug=True)
